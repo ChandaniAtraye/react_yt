4af55d1ea612b9be82874eefa57198d02547f760
Chandani Atraye <chandaniatraye5010@gmail.com>
2026-09-02 15:03:12 +0530

    notes app completed

--- 09-notes-app/src/App.jsx
+++ 09-notes-app/src/App.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { X } from 'lucide-react';
 import { SquarePen } from 'lucide-react';
 
 const App = () => {
   const [title, setTitle] = useState('')
   const [details, setDetails] = useState('')
-  const [task, setTask] = useState([])
+  
+  const [task, setTask] = useState(()=>{
+    return JSON.parse(localStorage.getItem("task")) || [] });
 
+    useEffect(()=>{
+      localStorage.setItem("task",JSON.stringify(task));
+    },[task]);
 
   const submitHandler = (e) => {
     e.preventDefault()
@@ -42,7 +47,7 @@ const App = () => {
         <h1 className='font-bold text-2xl '>Add Notes</h1>
 
         {/* first input */}
-        <input type='text' className='px-3 py-4 mt-2 border-2 rounded-2xl font-medium text-[16px] outline-none'
+        <input type='text' required className='px-3 py-4 mt-2 border-2 rounded-2xl font-medium text-[16px] outline-none'
           placeholder='Enter Task Heading'
           value={title}
           onChange={(e) => {
@@ -51,7 +56,7 @@ const App = () => {
         />
 
         {/* details of input */}
-        <textarea type='text' className='px-3 py-4  h-32 border-2 rounded-2xl font-medium text-[16px] outline-none'
+        <textarea type='text' required className='px-3 py-4  h-32 border-2 rounded-2xl font-medium text-[16px] outline-none'
           placeholder='Enter Details'
           value={details}
           onChange={(e) => {
@@ -64,21 +69,21 @@ const App = () => {
       </form>
 
        {/* notes card  section*/}
-      <div className=" lg:w-1/2 lg:border-l-2 overflow-auto  p-10">
-        <h1 className='font-bold text-2xl '>Your Notes</h1>
-        <div className='flex flex-wrap gap-5 justify-center items-start mt-5 '>
+      <div className=" lg:w-1/2 lg:border-l-2  overflow-auto  p-10">
+        <h1 className='font-bold text-2xl '>Recent Notes</h1>
+        <div className='flex flex-wrap gap-5 justify-start items-center mt-5 '>
           {task.map(function (elem, idx) {
 
-            return <div key={idx} className=" relative w-full sm:w-40 h-47  p-4 mt-2 bg-white rounded-2xl text-black bg-cover bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlM1LNDP-7nzJ8iIj91Ysvni7TOYk3JcQIHOk1LWJK9Q&s=10')]">
+            return <div key={idx} className=" card pt-6 relative w-full sm:w-45 h-50  p-4 mt-2 rounded-2xl  text-black bg-cover bg-bottom  bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlM1LNDP-7nzJ8iIj91Ysvni7TOYk3JcQIHOk1LWJK9Q&s=10')]">
 
               <h2 onClick={()=>{
                 deleteNote(idx)
-              }} className='absolute top-1 right-1  active:scale-95 '><X /></h2>
-               {/* <h2 onClick={()=>{
+              }} className='absolute top-1.5 right-1 hover:text-red-600  active:scale-95'><X size={20} /></h2>
+               <h2 onClick={()=>{
                 editNote(idx)
-              }} className='absolute top-1.5 right-8  active:scale-95 '><SquarePen size={20} strokeWidth={1.8} /></h2> */}
-              <h3 className='leading-tight font-medium text-xl font-serif'>{elem.title}</h3>
-              <p className='mt-3 leading-tight text-[16px] text-gray-500 font-medium' >{elem.details}</p>
+              }} className='absolute top-2 right-7  active:scale-95 '><SquarePen size={17} strokeWidth={1.8} /></h2>
+              <h3 className='leading-tight p-1 font-medium text-[18px] font-serif'>{elem.title}</h3>
+              <p className='mt-1 p-1 leading-tight text-[16px] text-gray-500 font-medium' >{elem.details}</p>
             </div>
           })}
         </div>
